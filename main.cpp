8dba285c23c58f8fe8af69220bf8da1c0babb62a
Listener4031 <21301050221@m.fudan>
2022-09-21 11:07:05 +0800

    test001

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <string>
+#include <cstring>
+
+#include "Bplustree.h"
+
+int main(){
+    std::cout << "Adder!" << std::endl;
+    int a;
+    int b;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << a+b << std::endl;
+    return 0;
+}
